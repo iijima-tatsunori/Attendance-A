13db5fcc6ca7504a677bfcb4159bf77914006ba5
iijima-tatsunori <himekawamaki45@gmail.com>
2020-07-10 06:28:30 +0000

    Finish site-infomation-edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,7 +73,13 @@ class UsersController < ApplicationController
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
-      redirect_to @user
+      
+      if current_user.admin?
+        redirect_to users_url
+      else
+        redirect_to @user
+      end
+      
     else
       render :edit
     end
